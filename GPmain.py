82aca668b157409e2ff3144350fcbe715d0d0ab8
GDcheerios <minecreeper0913@gmail.com>
2023-04-22 14:12:27 -0500

    added GPmain.py

---
+++ GPmain.py
@@ -0,0 +1,10 @@
+class GPSystem:
+    def __init__(self):
+        self.version = "1.0"
+        print("You're using GPSystem version ", self.version)
+
+
+
+if __name__ == '__main__':
+    program = GPSystem()
+    print()
