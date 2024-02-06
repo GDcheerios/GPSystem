2e29830ae58fde144d4686613b650c35052c0e77
GDcheerios <minecreeper0913@gmail.com>
2024-02-06 03:19:51 -0600

    updated version

--- GPSystem/GPmain.py
+++ GPSystem/GPmain.py
@@ -6,7 +6,7 @@ except ImportError:
 
 class GPSystem:
     rater = GPRater()
-    version = "1.5.2"
+    version = "1.6"
 
     def __init__(self):
         print("You're using GPSystem version ", GPSystem.version)
