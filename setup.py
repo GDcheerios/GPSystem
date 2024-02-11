eb413351854a1f79054a54a47c1a33d96ee18790
GDcheerios <minecreeper0913@gmail.com>
2024-02-11 08:09:16 -0600

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GPSystem',
-    version='1.8',
+    version='1.9',
     packages=['GPSystem'],
     url='https://github.com/GDcheeriosYT/GPSystem',
     license='UNKOWN',
