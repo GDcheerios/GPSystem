f84355d094ef8031edaaf79980b084891010a8bc
GDcheerios <minecreeper0913@gmail.com>
2024-10-27 07:12:59 -0500

    fix version

--- GPSystem/GPmain.py
+++ GPSystem/GPmain.py
@@ -6,7 +6,7 @@ except ImportError:
 
 class GPSystem:
     rater = GPRater()
-    version = "2.1.0"
+    version = "2.1.1"
 
     def __init__(self):
         print("You're using GPSystem version ", GPSystem.version)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='GPSystem',
-    version='2.0',
+    version='2.1.1',
     packages=['GPSystem'],
     url='https://github.com/GDcheeriosYT/GPSystem',
     license='UNKOWN',
